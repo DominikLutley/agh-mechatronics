e0e8af42116a31998f47a3a8fa2da37b916991ba
DominikLutley <domlutley@gmail.com>
2021-03-17 13:55:38 +0100

    Change of angle

--- start_simulating1.ino
+++ start_simulating1.ino
@@ -24,13 +24,13 @@ void loop()
 {
   // sweep the servo from 0 to 180 degrees in steps
   // of 1 degrees
-  for (pos = 0; pos <= 180; pos += 1) {
+  for (pos = 0; pos <= 360; pos += 1) {
     // tell servo to go to position in variable 'pos'
     servo_9.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
   }
-  for (pos = 180; pos >= 0; pos -= 1) {
+  for (pos = 180; pos >= 360; pos -= 1) {
     // tell servo to go to position in variable 'pos'
     servo_9.write(pos);
     // wait 15 ms for servo to reach the position
